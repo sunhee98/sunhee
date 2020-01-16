71d27124ba8331c6201756004c3d2e747c337681
Jung Sun Hee <sunhee98@naver.com>
2020-01-16 17:09:48 +0900

    구구단 문제

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log('Hello');
+function gugudan(start, end) {
+  for (let i = start; i <= end; i++) {
+    //console.log(i + '단');
+    console.log(`${i}단`);
+
+    for (let j = 1; j < 10; j++) {
+      console.log(`${i} * ${j} = ${i * j}`);
+    }
+    console.log(``);
+  }
+}
+gugudan(3, 6);
